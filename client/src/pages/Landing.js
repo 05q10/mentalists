f204e27cb60bbae0f9c12bbfee0febf82d4627f9
Ria Talsania <riatalsania0510@gmail.com>
2025-03-09 11:49:59 +0530

    l

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -21,7 +21,7 @@ const Landing = () => {
       {/* Button */}
       <button
         onClick={() => navigate("/comm")}
-        className="px-10 py-4 text-2xl font-bold uppercase text-purple-500 border-4 border-purple-600 rounded-lg shadow-lg transition-transform transform hover:scale-110 hover:shadow-purple-500 hover:shadow-md glow"
+        className="px-10 py-4 text-2xl font-bold uppercase text-purple-500 border-4 border-purple-600 rounded-lg shadow-lg transition-transform transform hover:scale-110 hover:shadow-purple-500 hover:shadow-md glow font-serif tracking-widest"
       >
         GigWiz<br></br>
         Join the Hub
